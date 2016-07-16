32bf9cecc1b46af96c14bb853487d6e4ebf6bd56
prgrmeval8r <rachelgita@gmail.com>
2016-07-16 11:47:29 -0700

    set up import for descriptor dict

---
+++ descript_dict.py
@@ -0,0 +1,51 @@
+#Dictionary of place descriptions
+descript = dict()
+    
+descript['front_yard'] = """
+A few desperate weeds struggle in the dusty ground. You look closer and recognize 
+ragged lavender plant, a tired rose bush, and realize that this was once a beautiful garden. 
+Now there is more trash here than plants. 
+"""
+descript['notice_board'] = """
+A large public notice board stands in the middle of the wide field. You wonder why the builder 
+decided to put it out here far from any road or town.
+Clearly a few people once came here, because there are a few colorful pages pinned to the board.
+You also notice extra pins, and a pad of blank paper with a pencil on a thin green ribbon 
+next to the board.
+"""
+descript['front_door'] = """
+The porch is slanting away from the house, it's likely some of the floor boards are rotten.
+You step carefully and notice there is an old mailbox next to a very large front door. 
+The windows are boarded up. You cannot see in.
+"""
+descript['south_field'] = """
+The field stretches out before you.
+"""
+descript['inside_house'] = """
+Thick dust covers everything. You cough as your eyes slowly accustom to the dim room.
+
+A table with a broken leg and knocked over chair were abandoned in the western corner. 
+A sink and cabinet sits along the north - east wall, and you can hear scrabbling of 
+some small animals.
+In the south corner is a large, dirty steamer trunk. Sitting on the trunk is a huge troll.
+"""
+descript['front_yard2'] = """
+Green flowering herbs and bushes wave in the slight breeze. You can smell the light fragrance
+of many flowers and hear the industrious buzzing of bees around you.
+Many more notes have been added to yours on the public gratitude board!
+"""
+descript['front_door2'] = """
+The front door is painted deep green with little carvings of ivy and berries in the corner.
+"""
+
+descript['inside_house2'] = """
+You look around an notice that the boards have disappeared from the window and you are
+now in a tiny home.
+
+A polished oak table and chair with a blue cushion fits into the west corner. 
+There is a bowl of fruit by  sink and cabinets are bright yellow. 
+
+And in the south corner the steamer trunk has been polished. Its leather gleams a rich dark
+brown, and its brass fittings sparkle in the sunlight.
+
+"""
