fbfaf6fb7ffce1461e70e84f6f4e529c9778c806
prgrmeval8r <prgrmeval8r@users.noreply.github.com>
2016-07-16 22:00:59 -0700

    added butterfly_wing (#2)
    * beginning of elf activity
    *added butterfly_wing activity for cleaning trash
    *moved descriptions and notes to separate file for all dicts
    *added south_field (if not added earlier)

--- Eunicegame.py
+++ Eunicegame.py
@@ -1,4 +1,7 @@
+#all imports should be at top of file
 import sys
+#import dictionary of story text
+from all_text_dicts import *
 
 #Saving Debug feature for when I figure out how I want to use it
 #DEBUG = False
@@ -31,65 +34,13 @@ You decide its time to begin making a change.
 prompt = '>'
 print intro
 
-#this should work - why doesnt it work? 
-paper_bird = None
+#Global variable for paper birds  
+paper_bird = 0
+butterfly_wing = 0
 
 #location variable is a context state. This starts game at 'front_yard'
 location = 'front_yard'
 
-#create dictionary of place descriptions
-descript = dict()
-    
-descript['front_yard'] = """
-A few desperate weeds struggle in the dusty ground. You look closer and recognize 
-ragged lavender plant, a tired rose bush, and realize that this was once a beautiful garden. 
-Now there is more trash here than plants. 
-"""
-descript['notice_board'] = """
-A large public notice board stands in the middle of the wide field. You wonder why the builder 
-decided to put it out here far from any road or town.
-Clearly a few people once came here, because there are a few colorful pages pinned to the board.
-You also notice extra pins, and a pad of blank paper with a pencil on a thin green ribbon 
-next to the board.
-"""
-descript['front_door'] = """
-The porch is slanting away from the house, it's likely some of the floor boards are rotten.
-You step carefully and notice there is an old mailbox next to a very large front door. 
-The windows are boarded up. You cannot see in.
-"""
-descript['south_field'] = """
-The field stretches out before you.
-"""
-descript['inside_house'] = """
-Thick dust covers everything. You cough as your eyes slowly accustom to the dim room.
-
-A table with a broken leg and knocked over chair were abandoned in the western corner. 
-A sink and cabinet sits along the north - east wall, and you can hear scrabbling of 
-some small animals.
-In the south corner is a large, dirty steamer trunk. Sitting on the trunk is a huge troll.
-"""
-descript['front_yard2'] = """
-Green flowering herbs and bushes wave in the slight breeze. You can smell the light fragrance
-of many flowers and hear the industrious buzzing of bees around you.
-Many more notes have been added to yours on the public gratitude board!
-"""
-descript['front_door2'] = """
-The front door is painted deep green with little carvings of ivy and berries in the corner.
-"""
-
-descript['inside_house2'] = """
-You look around an notice that the boards have disappeared from the window and you are
-now in a tiny home.
-
-A polished oak table and chair with a blue cushion fits into the west corner. 
-There is a bowl of fruit by  sink and cabinets are bright yellow. 
-
-And in the south corner the steamer trunk has been polished. Its leather gleams a rich dark
-brown, and its brass fittings sparkle in the sunlight.
-
-"""
-
-#create look function that accesses dictionary
 #its better to specifically pass in the the variables you need in given function
 def look(loc_to_describ):
     #use message not print because print is *very* specific and 
@@ -97,9 +48,19 @@ def look(loc_to_describ):
     #we pass the same name for an argument below because its going to take 'front_yard' and print 'front_yard'
     message(descript[loc_to_describ],loc_to_describ)
 
-def process_front_yard(command):
+def process_front_yard(command, butterfly_wing):
     l = 'front_yard'   
-    if command in ['go inside', 'go in house', 'go in the house', 'go in', 'enter house', 
+    if command in ['clean trash', 'clean up trash', 'look through trash', 'clean up',
+                    'clean yard']:
+        l = 'front_yard'
+        message ('Its a huge job, but you organize some of the broken objects into piles' +
+                'and clear some space around the plants. Beneath the trash you find' +
+                'a delicate butterfly wing. You keep it for later.', l)
+                #butterfly wing count     
+        butterfly_wing = butterfly_wing + 1
+        print 'Butterfly wings: ', butterfly_wing    
+        
+    elif command in ['go inside', 'go in house', 'go in the house', 'go in', 'enter house', 
                     'open door', 'walk inside', 'walk inside house']:
         l = 'front_yard'
         message ('You are too far from the house', l)
@@ -118,13 +79,14 @@ def process_front_yard(command):
                     'walk towards board', 'walk west', 'go west']:
         l = 'notice_board'
         message ('You walk to the notice board. It has a handful of brightly colored papers' + 
-                'with different handwriting. The pages look old and weather stained.', l)      
+                'with different handwriting. There is a blank note pad and pencil propped' + 
+                'against the left post.', l)      
     elif command in ['go south', 'walk field', 'walk south', 'walk to the south']:
         l = 'south_field'
         message ('The field stretches on and on before you.', l)
     else:
         message ('We could not parse that. You cannot ' + command, l)
-    return l
+    return l, butterfly_wing
     
         
 def process_notice_board(command, paper_bird):
@@ -133,24 +95,11 @@ def process_notice_board(command, paper_bird):
                     'read notice board', 'read the notice board', 'read notes on board', 
                     'read the notes on board', 'read note']:
         l = 'notice_board'
-        message ("""You move closer to read the colorful notes. They are pages of a 
-gratitude journal. One yellow note reads:
-        
-Three things I am grateful for today: 
-    my cats, quiet time to sip tea, laughing on the phone with a friend
-I feel these emotions when I think of these things: 
-    loved, content, joyful
-""", l)
+        message (gratitude_notes['note1'], l)
     elif command in ['read another', 'read another note', 'another', 'read another gratitude',
-                    'read another gratitice note']:
+                    'read another gratitude note']:
             l = 'notice_board'
-            message ("""An rose-colored paper reads:
-
-Three things I am grateful for today: 
-    morning commute without meeting any trolls or dragons, my children, sunny weather
-I feel these emotions when I think of these things: 
-    comfortable, inspired, happy
-""", l) 
+            message (gratitude_notes['note2'], l) 
     elif command in ['write note', 'write a note', 'write gratitude note', 'write a gratitude note',
                     'write gratitude', 'add note', 'add a note', 'add a gratitude note', 
                     'post gratitude note']:
@@ -167,14 +116,9 @@ I feel these emotions when I think of these things:
 Thank you for sharing. 
 When you post your gratitude note on the board, a tiny folded paper bird
 flutters down from above you. You keep it for later. """ % (grat, feelgrat) 
-#origami count
-    #count in the if statement every time there is an origami bird and save that number
-    #something to do with return?         
-        if paper_bird is None:
-            paper_bird = 1
-        else:
-            paper_bird = paper_bird + 1
-        print paper_bird    
+#origami count       
+        paper_bird = paper_bird + 1
+        print 'Origami birds: ', paper_bird    
     elif command in ['walk to front_yard', 'walk to the front_yard', 'walk east',
                     'walk to the east', 'walk back to house', 'walk back to the house']:
         l = 'front_yard'
@@ -225,9 +169,10 @@ def process_front_door(command):
         message ('You gingerly open the mailbox and peer inside. There is a sudden rustling' +
                 'and a pair of beady eyes glare at you from a tiny wrinkled face. The elf,' +
                 'for it has narrow pionty ears, is not happy to see you and screeches loudly' +
-                'BAAAAAAAA', l) 
+                'BAAAAAAAA!!', l) 
     #activity with elf
     #open door - is it locked first?
+    #paper birds would get key, something else should also give key NOT only birds
     #use key on door - how can a program 'have' a key or 'have' origami bird?
     else:
         message ('We could not parse that. You cannot ' + command, l)
@@ -238,6 +183,7 @@ def process_inside_house(command):
     if command in ['leave', 'go to door', 'go to porch', 'go outide']:
         l = 'front_door'
         message ('You leave the house closing the door behind you', l)
+    return l
 
 #while True is True - forever loop
 while True:
@@ -248,7 +194,7 @@ while True:
     elif command in ['exit', 'quit']:
         break
     elif location == 'front_yard':
-        location = process_front_yard(command)
+        (location, butterfly_wing) = process_front_yard(command, butterfly_wing)
     elif location == 'notice_board':
         (location, paper_bird) = process_notice_board(command, paper_bird)
     elif location == 'south_field':
