a7418620e30f594abbdb4e72827bf1dbf430ce6e
prgrmeval8r <prgrmeval8r@users.noreply.github.com>
2016-09-21 13:29:37 -0700

    Add files via upload

--- all_text_dicts.py
+++ all_text_dicts.py
@@ -1,22 +1,146 @@
 #Dictionary of place descriptions
 descript = dict()
-    
-descript['front_yard'] = """
-A few desperate weeds struggle in the dusty ground. You look closer and recognize 
-ragged lavender plant, a tired rose bush, and realize that this was once a beautiful garden. 
-Now there is more trash here than plants. 
+
+descript['intro'] = """
+Welcome to Eunice.
+
+You are standing in a yard just south of an old, wood house. 
+At one time it was a dearly loved home, a place of comfort and laughter.
+Now it stands silent and empty, with its windows boarded up. 
+
+Rotten wooden boards, twisted metal, and old tires lie abandoned in the yard crowding the plants.
+It could use a little cleaning up.
+
+A little note is stuck on a rusty nail.
+
+You decide its time to begin making a change.
 """
+descript['yard_note'] = """
+The note reads:
+
+Welcome to Eunice, it is a shadow of its former glory.
+
+Use simple instructions to interact with this place and its strange inhabitants.
+To move around use cardinal compass directions or 'go' + cardinal compass direction
+
+Type 'look' to view your surroundings, and keep an eye out for possible changes.
+
+Type 'exit' to leave the game.
+
+Type 'investigate' + object to find out more about a specific thing.
+
+Remember, you will get as much from the game as you decide to put in to it.
+"""
+descript['front_yard'] = """
+This was once a beautiful garden.
+Now there is more trash on the ground than plants.
+It could use a clean up.
+
+You look closer and recognize ragged lavender plant, a tired rose bush.
+A note is stuck to a rose thorn.
+ 
+A field stretches out to the west, south, and east.
+North of you is a boarded-up house.
+"""
+descript['clean_yard'] = """
+Its a huge job, but you organize some of the broken objects into piles and 
+clear some space around the plants. Beneath the trash you find a delicate 
+butterfly wing. 
+
+You keep it for later.
+"""
+
+descript['east_field'] = """
+Tall grasses wave in the breeze. 
+The air smells green and wet.
+"""
+
+descript['easteast_field'] = """
+Mud squelches beneath your feet. 
+
+There is a light fragrance of rotting plants.
+Flies are buzzing in the air
+
+Further east you see something shimmering, like drops of water in the sun.
+"""
+descript['quaking_bog'] = """
+The air smells of mud and just a hint of sulphur.
+Moss, water, and thick marsh grasses mix at your feet
+
+Nearby you see a board posting the 'Park Rules and Regulations'.
+"""
+
+descript['bog_heart'] = """
+As you walk forward, on thick moss, the ground quakes slightly with each footfall.
+
+You are walking on water - or above it - on many layers of moss that have grown over
+to form a living raft on the surface of the bog.
+Who knows what treasures lie below the deep moss...
+
+A toad is nestled into the moss, as if he was sitting among many overstuffed pillows.
+He looks to be asleep, and regularly makes snoring croaking sounds.
+"""
+
+descript['bog_heart2'] = """
+You are standing on water - or above it - on many layers of moss that have grown over
+to form a living raft on the surface of the bog.
+Who knows what treasures lie below the deep moss...
+
+A toad is nestled into the moss, as if he was sitting among many overstuffed pillows.
+He is looking at you with interest.
+"""
+
+descript['west_field'] = """
+The west field is a barren dusty place. 
+
+The land quickly becomes hilly.
+
+In the far distance you can see the hills grow into jagged mountains that overwhelm the 
+horizon.
+
+To the west is a large wooden notice board.
+"""
+
 descript['notice_board'] = """
-A large public notice board stands in the middle of the wide field. You wonder why the builder 
-decided to put it out here far from any road or town.
-Clearly a few people once came here, because there are a few colorful pages pinned to the board.
+A large public notice board stands in the middle of the wide field. 
+
+You wonder why the builder decided to put it out here far from any road or town.
+Clearly people once came here, because there are two colorful notes pinned to the board.
+
 You also notice extra pins, and a pad of blank paper with a pencil on a thin green ribbon 
 next to the board.
+
+The notes are all about gratitude!
 """
 descript['front_door'] = """
 The porch is slanting away from the house, it's likely some of the floor boards are rotten.
-You step carefully and notice there is an old mailbox next to a very large front door. 
-The windows are boarded up. You cannot see in.
+
+You step carefully and notice there is an old mailbox and dirty welcome mat next to a 
+very large front door. 
+
+The windows of the house are boarded up. 
+You cannot see in.
+"""
+
+descript['maze_entrance'] = """
+You are at the entrance to a maze.
+You can go back (b) to return here to the maze entrance.
+"""
+
+descript['maze_begin'] = """
+You enter the maze passing by mossy rocks into the shadowy passageway that goes forward (f) or right (r)'
+You can go back (b) to return to the maze entrance.'
+"""
+descript['maze'] = """
+You are in a maze of twisty little stone passageways
+"""
+descript['north_field_east'] = """
+Grass covers the ground.
+You are in the north field east.
+"""
+descript['north_field_west'] = """
+Grass covers the ground.
+You are in the north field west.
 """
 descript['south_field'] = """
 The field stretches out before you.
@@ -25,20 +149,36 @@ descript['inside_house'] = """
 Thick dust covers everything. You cough as your eyes slowly accustom to the dim room.
 
 A table with a broken leg and knocked over chair were abandoned in the western corner. 
-A sink and cabinet sits along the north - east wall, and you can hear scrabbling of 
-some small animals.
+You can hear scrabbling of some small animals in the walls.
+
 In the south corner is a large, dirty steamer trunk. Sitting on the trunk is a huge troll.
+His face is drawn into a menacing grimace.
+He wrings his hands and cries, 'Go away! I'm much too anxious to deal with visitors!'
+
+At this point you notice a piece of paper lying on the ground.
 """
 descript['front_yard2'] = """
-Green flowering herbs and bushes wave in the slight breeze. You can smell the light fragrance
-of many flowers and hear the industrious buzzing of bees around you.
-Many more notes have been added to yours on the public gratitude board!
+With the trash cleaned up, the whole garden becomes just a little brighter.
+
+The rose bush has tiny pink buds, preparing to become flowers soon.
 """
 descript['front_door2'] = """
-The front door is painted deep green with little carvings of ivy and berries in the corner.
+The porch looks recently painted and repaired. 
+
+There is the same old mailbox and dirty welcome mat next to a very large front door. 
+The windows of the house are boarded up. 
+You cannot see in.
+"""
+descript['inside_house2'] = """
+The dust has cleared from inside the house.
+
+A table with a broken leg and knocked over chair still sit in the western corner. 
+
+In the south corner is a large, dirty steamer trunk. Sitting on the trunk is a huge troll.
+He has closed his eyes and is breathing very slowly.
 """
 
-descript['inside_house2'] = """
+descript['inside_house3'] = """
 You look around an notice that the boards have disappeared from the window and you are
 now in a tiny home.
 
@@ -47,7 +187,41 @@ There is a bowl of fruit by  sink and cabinets are bright yellow.
 
 And in the south corner the steamer trunk has been polished. Its leather gleams a rich dark
 brown, and its brass fittings sparkle in the sunlight.
+"""
 
+#Investigation Dictionary
+curiosity = dict()
+
+curiosity['garden'] = """
+The garden is dry and littered with forgotten mail, broken windows, pieces of wood.
+
+Maybe if someone cleaned up it would look better. 
+"""
+
+curiosity['garden2'] = """
+A faint perfume scents the air. Soothing lavender sways in the breeze, now that the trash is cleared.
+Thyme and creeping wintergreen add green to the ground.
+
+The rose bush is budding and will flower soon.
+"""
+
+curiosity['butterfly_wing'] = """
+The wing glitters in deep browns with facinful swirls of gold and blue.
+As you look, the wing begins to look almost tasty, but it is definitely not human food.
+"""
+
+curiosity['toad'] = """
+The toad is large and green and frowning. His eyes are closed and makes loud croaking noises every so often.
+
+As you look closer, you notice a curved lillypad, about the size of a plate, near the frog.
+'Advice given in exchange for snacks' is printed on the plate.
+
+The frog does look a little out of sorts, maybe he is hungry.
+"""
+
+curiosity['notice_board'] = """
+From here, it looks like the kind of notice board posted at country road crossings.
+Though there is no road in sight.
 """
 
 #Dictionary of gratitude notes 
@@ -58,9 +232,10 @@ gratitude_notes['note1'] = """
 One yellow note reads:
         
 Three things I am grateful for today: 
-    Agrimony tea, house elves, catching up with an old friend
+    Warm cup of tea, house elves, catching up with an old friend
+
 I feel these emotions when I think of these things: 
-    loved, content, joyful
+    cared for, content, joyful
 """
 
 gratitude_notes['note2'] = """
@@ -68,7 +243,203 @@ A rose-colored paper reads:
 
 Three things I am grateful for today: 
     morning commute free from trolls or dragons, my children, sunny weather
+
 I feel these emotions when I think of these things: 
-    comfortable, inspired, happy
-    
+    comfortable, loving, happy
 """  
+
+gratitude_notes['note_bird'] = """
+
+Thank you for sharing. 
+
+When you post your gratitude note on the board, a tiny folded paper bird
+flutters down from above you. 
+
+You keep it for later.
+"""
+
+gratitude_notes['end'] = """
+You walk on to explore west of the notice board in the waving grass beneath rugged mountains.
+
+Suddenly you see what the shadow of the mountains had hidden: the land falls away in
+a steep cliff so deep that the bottom is hidden from view. 
+
+You scramble back from the edge.
+There is now way to go further west.
+
+With a pounding heart, you turn back to the notice board.
+"""
+
+#Dictionary of elf speak
+elf_speak = dict()
+
+elf_speak['meet_elf'] = """
+You open the mailbox and peer inside. 
+
+There is a sudden rustling and a pair of beady eyes glare at you from a tiny wrinkled face. 
+
+The elf, for she has narrow pointy ears sticking up above a little green hat, is not happy to be distrubed.
+
+She screeches loudly: 'BAAAAAAAAAAAAA!!'
+"""
+
+elf_speak['elf_tongue'] = """
+The little house elf sticks out her tongue and turns his back on you
+"""
+
+elf_speak['get_bird'] = """
+The elf takes the paper bird, almost as large as he is, and places it carefully in a
+ corner of the mailbox.
+
+A ghost of a smile glides across his face.
+Around you, things are starting to brighten up.
+
+The elf says, 'That is very generous of you. 
+Its been so long since I met an actual human.
+
+Tell me about youself.'
+"""
+
+elf_speak['no_butterfly'] = """
+The elf peers at the delicate butterfly wing as it shines in the sun.
+
+'I have nothing to do with that' he says.
+
+And he sticks his tongue out at you and frowns.
+"""
+
+elf_speak['elf_talk'] = """
+'Hey, so what happened around here?' you ask.
+
+'Been going downhill since I dont know when'
+the elf replied, and crossed his arms.
+
+'This place needs help.'
+"""
+
+elf_speak['elf_help'] = """
+'Can I do anything to help?' you ask
+
+'Go in house and fix up a few things, but mind the troooooooooll!'
+
+Suddenly you find yourself inside the old house!
+"""
+
+#Dictionary of quaking bog  --include bog_heart too?
+bog_text = dict()
+
+bog_text['bog_rules'] = """
+Instructions and Rules:
+   - Drowning in the bog is forbidden 
+   - Watch out for the carnivourous plants 
+   - Do appreciate nature in this amazing ecosystem  
+"""
+
+bog_text['bog_a0'] = """
+You look around and notice that everywhere around you is full of living, growing
+things. 
+
+Dragonflies and butterflies and plain flies swoop between shrubs and grasses.
+
+"""
+bog_text['bog_a1'] = """
+Long-legged shorebirds poke around in the moss.
+
+The air smells green and rich.
+
+A sparkle catches your eye.
+
+"""
+bog_text['bog_a2'] = """
+A single dew drop balances on the edge of a curved grass leaf. 
+
+Slowly it grows in fullness, glinting like a little star.
+
+The weight pulles the thin grass down, down until it drops silently into the watery green at your feet.
+
+"""
+bog_text['bog_a3'] = """
+You take a deep breath.
+
+Breathe in and feel the breath fill from your belly to your ribs to your collarbones. 
+
+Exhaling the grass flutters from the wind of your breath.
+
+"""
+bog_text['bog_a4'] = """
+You feel happier, more relaxed, and ready to continue your exploration.
+
+You think, I will try practicing appreciating nature again the next time that I am outside.
+
+"""
+
+bog_text['bog_a5'] = """
+Then a deep, rasping croak interrupts your thoughts. It is coming from the heart of the bog!
+"""
+#http://greatergood.berkeley.edu/article/item/how_nature_makes_you_kinder_happier_more_creative
+#http://wilderness.org/blog/study-shows-time-outdoors-away-technology-can-boost-creativity
+#http://journals.plos.org/plosone/article?id=10.1371/journal.pone.0051474
+
+#frog discussion in the bog_heart
+bog_text['bog_toad'] = """
+Without opening his eyes, the toad unfurls his raspy tongue to delicately pick up the shiny butterfly wing.
+He crunches on it like a snack cracker.
+
+Then his eyes open, 'Oh, you. I have just the advice for you.'
+"""
+
+bog_text['toad_talk'] = """
+The toad recites this riddle:
+
+RIDDLE
+
+"""
+
+
+#Dictionary of the maze
+#maze_text = dict()
+#You enter the maze at the west, or east entrance
+# though the floor looked stable, in fact the rocks were an incredible
+#optical illusion and once you step on them you slip down a steep slide,
+#spinning as you go.
+#Finally, dizzy and disoriented, you come to a halt in a small piazza. There are stone
+#statues. You cannot climb up the way you came.
+#You can go forward, backward, right, or left. That is all.
+
+
+
+#Dictionary of troll speak
+troll_speak = dict()
+
+troll_speak ['anxiety_info'] = """
+Tips to Manage Anxiety and Stress
+
+ - Take a deep breath. Try to inhale and exhale slowly
+ 
+ - Try humor, a silly joke can go a long way to improve a troll's mood.
+ 
+ - Talk to someone. Some trolls find that humans are especially chatty.
+ 
+    more info at www.adaa.org
+"""
+
+troll_speak ['troll_talk'] = """
+'Is there anything you want to talk about?' you ask.
+
+The Troll growls, and then goes on a long rant about the state of Trollish politics,
+how he might have eaten a bad grub yesterday, and that he still has not found a good
+toenail care routine!
+
+Finally he winds down to the end, 'thanks, I feel much better getting that out!'
+"""
+
+troll_speak ['troll_joke'] = """
+You suddenly remember a few silly jokes, and tell one to the Troll
+"""
+
+troll_speak['troll_breath'] = """
+'How about we both try taking a deep breath,' you suggest
+"""
+
+
+
